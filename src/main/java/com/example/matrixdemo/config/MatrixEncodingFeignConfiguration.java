b66356facd70f40fe0de6b63eb6e738f31d9c4d1
Oliver Lockwood <olockwood@dalet.com>
2021-03-26 14:30:16 +0000

    Change configuration to avoid the need for a custom build of spring-cloud

--- src/main/java/com/example/matrixdemo/config/MatrixEncodingFeignConfiguration.java
+++ src/main/java/com/example/matrixdemo/config/MatrixEncodingFeignConfiguration.java
@@ -8,11 +8,21 @@
  */
 package com.example.matrixdemo.config;
 
+import feign.Contract;
 import feign.codec.Encoder;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.AnnotatedParameterProcessor;
+import org.springframework.cloud.openfeign.support.SpringMvcContract;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.web.bind.annotation.MatrixVariable;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -26,6 +36,28 @@ public class MatrixEncodingFeignConfiguration {
     public Encoder encoder(ObjectFactory<HttpMessageConverters> messageConverters) {
 
         return new MatrixVariableHandlingEncoder(messageConverters);
+    }
+
+
+    @Bean
+    public Contract feignContract(ConversionService feignConversionService,
+                                  List<AnnotatedParameterProcessor> parameterProcessors) {
+        SpringMvcContract contract =  new SpringMvcContract(parameterProcessors, feignConversionService);
+
+        try {
+            Field annotatedArgumentProcessorsField = SpringMvcContract.class.
+                    getDeclaredField("annotatedArgumentProcessors");
+            annotatedArgumentProcessorsField.setAccessible(true);
+            Map<Class<? extends Annotation>, AnnotatedParameterProcessor> annotatedArgumentProcessors =
+                    (Map) annotatedArgumentProcessorsField.get(contract);
+
+            annotatedArgumentProcessors.remove(MatrixVariable.class);
+
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+        return contract;
     }
 
 }
